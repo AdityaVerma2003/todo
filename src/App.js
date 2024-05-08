92c69329a320433613309c43137af34bac4af8fe
AdityaVerma2003 <av876777@gmail.com>
2024-05-08 09:51:02 +0530

    Todo list app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [note, setNote] = useState(" ")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+         <h1 className="heading"> Todo List  </h1>
+         <input type="text" placeholder="Enter Note" name="note" value={note}  onChange={(e)=> setNote(e.target.value) }/>
+      </div>
+
+      
     </div>
   );
 }
